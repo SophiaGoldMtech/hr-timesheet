b2e865047fd958576bc912374bcfeabb1a9804b6
Sophia Gold <Sophia.gold2920@stu.mtec.edu>
2024-09-25 09:48:57 -0600

    Added matButton, matIcon, and matToolbar imports

--- src/app/modules/material.module.ts
+++ src/app/modules/material.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButton, MatButtonModule } from '@angular/material/button';
+import { MatIcon, MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, MatToolbarModule, MatButtonModule, MatIconModule],
+  exports: [MatToolbarModule, MatButtonModule, MatIconModule],
 })
-export class MaterialModule { }
+export class MaterialModule {}
