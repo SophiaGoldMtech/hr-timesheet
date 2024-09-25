e1c26f81a16ab54e21d6535844b47ffae0877c7d
Sophia Gold <Sophia.gold2920@stu.mtec.edu>
2024-09-25 14:00:15 -0600

    Injecting router into departments component constructor

--- src/app/components/departments/departments.component.ts
+++ src/app/components/departments/departments.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DepartmentsService } from '../../services/departments.service';
 import { Department } from '../../interfaces/department';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-departments',
@@ -10,7 +11,10 @@ import { Department } from '../../interfaces/department';
 export class DepartmentsComponent implements OnInit {
   departments!: Department[];
 
-  constructor(private departmentsService: DepartmentsService) {}
+  constructor(
+    private departmentsService: DepartmentsService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.departments = this.departmentsService.departments;
