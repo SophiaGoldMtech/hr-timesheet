a9c5231583eef3ec7871be2873da8f0e075221d7
Sophia Gold <Sophia.gold2920@stu.mtec.edu>
2024-09-25 10:18:59 -0600

    Injecting departments service

--- src/app/components/departments/departments.component.ts
+++ src/app/components/departments/departments.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DepartmentsService } from '../../services/departments.service';
 
 @Component({
   selector: 'app-departments',
   templateUrl: './departments.component.html',
-  styleUrl: './departments.component.scss'
+  styleUrl: './departments.component.scss',
 })
-export class DepartmentsComponent {
+export class DepartmentsComponent implements OnInit {
+  constructor(private departmentsService: DepartmentsService) {}
 
+  ngOnInit(): void {}
 }
