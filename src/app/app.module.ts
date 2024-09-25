3c7a40b511b7e734fe85e963823ab73bb32c1b13
Sophia Gold <Sophia.gold2920@stu.mtec.edu>
2024-09-25 09:34:54 -0600

    Create department component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './modules/app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { DepartmentsComponent } from './components/departments/departments.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, DepartmentsComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [provideAnimationsAsync()],
   bootstrap: [AppComponent],
