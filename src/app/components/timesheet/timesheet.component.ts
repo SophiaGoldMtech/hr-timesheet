73654d82769c289b26a3da7a8a9d83aa63f33798
Sophia Gold <Sophia.gold2920@stu.mtec.edu>
2024-09-25 14:06:44 -0600

    Injecting activated route

--- src/app/components/timesheet/timesheet.component.ts
+++ src/app/components/timesheet/timesheet.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-timesheet',
   templateUrl: './timesheet.component.html',
-  styleUrl: './timesheet.component.scss'
+  styleUrl: './timesheet.component.scss',
 })
-export class TimesheetComponent {
+export class TimesheetComponent implements OnInit {
+  constructor(private route: ActivatedRoute) {}
 
+  ngOnInit(): void {}
 }
